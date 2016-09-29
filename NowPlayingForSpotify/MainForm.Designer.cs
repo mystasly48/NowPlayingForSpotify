00abd7028b58c139013c5d8555272ea14479198d
Maisu <mystasly48@gmail.com>
2016-09-30 00:08:52 +0900

    Added: main form

---
+++ NowPlayingForSpotify/MainForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace NowPlayingForSpotify {
+    partial class MainForm {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.albumPicture = new System.Windows.Forms.PictureBox();
+            this.trackLabel = new System.Windows.Forms.Label();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.albumLabel = new System.Windows.Forms.Label();
+            this.trackLink = new System.Windows.Forms.LinkLabel();
+            this.artistLink = new System.Windows.Forms.LinkLabel();
+            this.albumLink = new System.Windows.Forms.LinkLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.albumPicture)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // albumPicture
+            // 
+            this.albumPicture.Location = new System.Drawing.Point(12, 13);
+            this.albumPicture.Name = "albumPicture";
+            this.albumPicture.Size = new System.Drawing.Size(100, 100);
+            this.albumPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.albumPicture.TabIndex = 1;
+            this.albumPicture.TabStop = false;
+            // 
+            // trackLabel
+            // 
+            this.trackLabel.AutoSize = true;
+            this.trackLabel.Location = new System.Drawing.Point(119, 13);
+            this.trackLabel.Name = "trackLabel";
+            this.trackLabel.Size = new System.Drawing.Size(36, 12);
+            this.trackLabel.TabIndex = 2;
+            this.trackLabel.Text = "Track:";
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Location = new System.Drawing.Point(118, 36);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(40, 12);
+            this.artistLabel.TabIndex = 3;
+            this.artistLabel.Text = "Artist: ";
+            // 
+            // albumLabel
+            // 
+            this.albumLabel.AutoSize = true;
+            this.albumLabel.Location = new System.Drawing.Point(118, 59);
+            this.albumLabel.Name = "albumLabel";
+            this.albumLabel.Size = new System.Drawing.Size(43, 12);
+            this.albumLabel.TabIndex = 4;
+            this.albumLabel.Text = "Album: ";
+            // 
+            // trackLink
+            // 
+            this.trackLink.AutoSize = true;
+            this.trackLink.Location = new System.Drawing.Point(162, 13);
+            this.trackLink.Name = "trackLink";
+            this.trackLink.Size = new System.Drawing.Size(0, 12);
+            this.trackLink.TabIndex = 6;
+            this.trackLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.titleLink_LinkClicked);
+            // 
+            // artistLink
+            // 
+            this.artistLink.AutoSize = true;
+            this.artistLink.Location = new System.Drawing.Point(162, 36);
+            this.artistLink.Name = "artistLink";
+            this.artistLink.Size = new System.Drawing.Size(0, 12);
+            this.artistLink.TabIndex = 7;
+            this.artistLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.artistLink_LinkClicked);
+            // 
+            // albumLink
+            // 
+            this.albumLink.AutoSize = true;
+            this.albumLink.Location = new System.Drawing.Point(162, 59);
+            this.albumLink.Name = "albumLink";
+            this.albumLink.Size = new System.Drawing.Size(0, 12);
+            this.albumLink.TabIndex = 8;
+            this.albumLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.albumLink_LinkClicked);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "NowPlaying for Spotify";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // hideBtn
+            // 
+            this.hideBtn.Location = new System.Drawing.Point(316, 90);
+            this.hideBtn.Name = "hideBtn";
+            this.hideBtn.Size = new System.Drawing.Size(42, 23);
+            this.hideBtn.TabIndex = 9;
+            this.hideBtn.Text = "Hide";
+            this.hideBtn.UseVisualStyleBackColor = true;
+            this.hideBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(121, 96);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Text File";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.Location = new System.Drawing.Point(249, 90);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(61, 23);
+            this.settingsBtn.TabIndex = 11;
+            this.settingsBtn.Text = "Settings";
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            this.settingsBtn.Click += new System.EventHandler(this.settingsBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 123);
+            this.Controls.Add(this.settingsBtn);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.hideBtn);
+            this.Controls.Add(this.albumLink);
+            this.Controls.Add(this.artistLink);
+            this.Controls.Add(this.trackLink);
+            this.Controls.Add(this.albumLabel);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.trackLabel);
+            this.Controls.Add(this.albumPicture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "NowPlaying for Spotify";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.albumPicture)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox albumPicture;
+        private System.Windows.Forms.Label trackLabel;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.Label albumLabel;
+        private System.Windows.Forms.LinkLabel trackLink;
+        private System.Windows.Forms.LinkLabel artistLink;
+        private System.Windows.Forms.LinkLabel albumLink;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button hideBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button settingsBtn;
+    }
+}
+
---
+++ NowPlayingForSpotify/MainForm.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using CoreTweet;
+using NowPlayingForSpotify.Properties;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+
+namespace NowPlayingForSpotify {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        #region Reference
+
+        public static string AppName = "NowPlaying for Spotify";
+        Settings settings = new Settings();
+        string Spotify = "Spotify";
+        Tokens twitter;
+        SpotifyLocalAPI spotify = new SpotifyLocalAPI();
+        Track currentTrack;
+        bool IsPlaying = false;
+        String LastTweet = "";
+        Boolean CreateTextFile = false;
+        DateTime StartTime;
+        SplashForm splash = new SplashForm();
+
+        #endregion
+
+        #region Other
+
+        private void ShowSplash() {
+            splash.Show();
+            splash.Refresh();
+        }
+
+        private void CloseSplash() {
+            splash.Close();
+            splash.Dispose();
+            this.Activate();
+        }
+
+        private void OpenLink(LinkLabel label) {
+            Process.Start(label.Tag.ToString());
+        }
+
+        private void init() {
+            SettingsInit();
+            TwitterInit();
+            SpotifyInit();
+        }
+
+        private void SettingsInit() {
+            LastTweet = settings.LastTweet;
+            CreateTextFile = settings.CreateTextFile;
+
+            this.Location = settings.StartPosition;
+            checkBox1.Checked = CreateTextFile;
+        }
+
+        private void SaveSettings() {
+            settings.LastTweet = LastTweet;
+            //settings.CreateTextFile = CreateTextFile;
+            settings.StartPosition = this.Location;
+            settings.Save();
+        }
+
+        private void EnvironmentExit() {
+            Environment.Exit(0);
+        }
+
+        private void Exit() {
+            Application.Exit();
+        }
+
+        private void Message(string msg) {
+            MessageBox.Show(msg, AppName);
+        }
+
+        private void Message(string msg, string title) {
+            MessageBox.Show(msg, title);
+        }
+
+        private void HideNotifyIcon() {
+            this.Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void ShowNotifyIcon() {
+            this.Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void CreateNowPlayingFile() {
+            if (CreateTextFile) {
+                var path = "NowPlaying.txt";
+                var space = "     ";
+                var str = artistLink.Text + " - " + trackLink.Text + space;
+                using (var sr = new StreamWriter(path, false, Encoding.UTF8)) {
+                    sr.Write(str);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Form
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            ShowSplash();
+            init();
+            CloseSplash();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            SendTweet();
+            timer1.Stop();
+        }
+
+        private void titleLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            OpenLink(trackLink);
+        }
+
+        private void artistLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            OpenLink(artistLink);
+        }
+
+        private void albumLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            OpenLink(albumLink);
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e) {
+            HideNotifyIcon();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Exit();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
+            HideNotifyIcon();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            ShowNotifyIcon();
+        }
+
+        private void settingsBtn_Click(object sender, EventArgs e) {
+            FormatCustomize form = new FormatCustomize();
+            form.ShowDialog();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+            SaveSettings();
+            notifyIcon1.Visible = false;
+            notifyIcon1.Dispose();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            CreateTextFile = checkBox1.Checked;
+            CreateNowPlayingFile();
+        }
+
+        #endregion
+
+        #region Twitter
+
+        private void TwitterInit() {
+            if (String.IsNullOrEmpty(settings.AccessToken) || String.IsNullOrEmpty(settings.AccessSecret)) {
+                var session = OAuth.Authorize(Keys.ConsumerKey, Keys.ConsumerSecret);
+                Process.Start(session.AuthorizeUri.AbsoluteUri);
+                InputBox input = new InputBox();
+                while (true) {
+                    input.ShowDialog();
+                    var pin = input.Pin;
+                    if (pin.Equals("exit")) {
+                        Environment.Exit(0);
+                    }
+                    try {
+                        twitter = session.GetTokens(pin);
+                        break;
+                    } catch (TwitterException ex) {
+                        if (ex.Message.Equals("Error processing your OAuth request: Invalid oauth_verifier parameter")) {
+                            continue;
+                        }
+                        throw;
+                    }
+                }
+                settings.AccessToken = twitter.AccessToken;
+                settings.AccessSecret = twitter.AccessTokenSecret;
+                settings.Save();
+            } else {
+                twitter = Tokens.Create(Keys.ConsumerKey, Keys.ConsumerSecret, settings.AccessToken, settings.AccessSecret);
+            }
+            if (String.IsNullOrEmpty(LastTweet)) {
+                var timeline = twitter.Statuses.UserTimeline(screen_name: twitter.Account.UpdateProfile().ScreenName, count: 1, exclude_replies: true, include_rts: false);
+                foreach (var tweet in timeline) {
+                    LastTweet = tweet.Text;
+                }
+            }
+        }
+
+        private void SendTweet() {
+            var track = trackLink.Text;
+            var artist = artistLink.Text;
+            var album = albumLink.Text;
+            var datetime = DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss");
+            var trackurl = trackLink.Tag.ToString().Replace("spotify:track:", "https://open.spotify.com/track/");
+            var artisturl = artistLink.Tag.ToString().Replace("spotify:artist:", "https://open.spotify.com/artist/");
+            var albumurl = albumLink.Tag.ToString().Replace("spotify:album:", "https://open.spotify.com/album/");
+            var newline = Environment.NewLine;
+            var tweet = settings.TweetFormat;
+
+            track = ShortenTweet(track, 30);
+            artist = ShortenTweet(artist, 30);
+            album = ShortenTweet(album, 30);
+            // url 60
+            // other 30
+            // format 50
+
+            tweet = ReplaceFormat(tweet, "%track%", track);
+            tweet = ReplaceFormat(tweet, "%artist%", artist);
+            tweet = ReplaceFormat(tweet, "%album%", album);
+            tweet = ReplaceFormat(tweet, "%trackurl%", trackurl);
+            tweet = ReplaceFormat(tweet, "%artisturl%", artisturl);
+            tweet = ReplaceFormat(tweet, "%albumurl%", albumurl);
+            tweet = ReplaceFormat(tweet, "%datetime%", datetime);
+            tweet = ReplaceFormat(tweet, "%newline%", newline);
+
+            tweet = ShortenTweet(tweet, 140);
+
+            try {
+                if (!LastTweet.Equals(artist + "|" + track + "|" + trackurl)) {
+                    twitter.Statuses.Update(status: tweet);
+                    LastTweet = artist + "|" + track + "|" + trackurl;
+                }
+            } catch (TwitterException ex) {
+                if (ex.Message.Equals("Status is a duplicate.")) {
+                    return;
+                }
+                throw;
+            } catch (WebException ex) {
+                if (ex.Message.Equals("要求は中止されました: SSL/TLS のセキュリティで保護されているチャネルを作成できませんでした")) {
+                    // 不明
+                }
+                throw;
+            } catch (NullReferenceException ex) {
+                if (ex.Message.Equals("オブジェクト参照がオブジェクト インスタンスに設定されていません。")) {
+                    // 不明？
+                }
+                throw;
+            } catch (Exception ex) {
+                if (ex.Message.Contains("またはその依存関係の１つが読み込めませんでした。見つかったアセンブリのマニフェスト定義またはアセンブリ参照に一致しません。")) {
+                    Message(ex.Message, "Error: Newtonsoft.Json");
+                    // 不明
+                }
+                throw;
+            }
+        }
+
+        private string ReplaceFormat(string tweet, string par2, string par3) {
+            if (tweet.Contains(par2)) {
+                tweet = tweet.Replace(par2, par3);
+            }
+            return tweet;
+        }
+
+        private string ShortenTweet(string tweet, int length) {
+            if (tweet.Length > length) {
+                tweet = tweet.Substring(0, length - 2) + "..";
+            }
+            return tweet;
+        }
+
+        #endregion
+
+        #region Spotify
+
+        private void SpotifyInit() {
+            spotify.OnTrackChange += OnTrackChange;
+            spotify.OnPlayStateChange += OnPlayStateChange;
+
+            if (!SpotifyLocalAPI.IsSpotifyRunning()) {
+                Message("Spotify が起動していません。");
+                EnvironmentExit();
+                return;
+            }
+            if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning()) {
+                Message("SpotifyWebHelper が起動していません。");
+                EnvironmentExit();
+                return;
+            }
+
+            var successful = spotify.Connect();
+            if (successful) {
+                spotify.ListenForEvents = true;
+            } else {
+                Message("Spotify に接続できませんでした。" + Environment.NewLine + "後ほどお試しください。");
+                EnvironmentExit();
+            }
+
+            var status = spotify.GetStatus();
+            if (status == null) {
+                return;
+            }
+
+            if (status.Track != null) {
+                IsPlaying = status.Playing;
+                UpdateTrack(status.Track);
+            }
+        }
+
+        private void OnTrackChange(object sender, TrackChangeEventArgs e) {
+            UpdateTrack(e.NewTrack);
+        }
+
+        private void OnPlayStateChange(object sender, PlayStateEventArgs e) {
+            IsPlaying = e.Playing;
+            if (IsPlaying) {
+                UpdateTrack(currentTrack);
+            } else {
+                timer1.Stop();
+            }
+        }
+
+        private delegate void CallbackTrack(Track track);
+        private async void UpdateTrack(Track track) {
+            if (InvokeRequired) {
+                var method = new CallbackTrack(UpdateTrack);
+                Invoke(method, new object[] { track });
+            }
+
+            currentTrack = track;
+
+            if (track.IsAd()) {
+                AdUpdate();
+                return;
+            }
+
+            albumPicture.Image = await track.GetAlbumArtAsync(AlbumArtSize.Size160);
+            trackLink.Text = track.TrackResource.Name;
+            trackLink.Tag = track.TrackResource.Uri;
+            artistLink.Text = track.ArtistResource.Name;
+            artistLink.Tag = track.ArtistResource.Uri;
+            albumLink.Text = track.AlbumResource.Name;
+            albumLink.Tag = track.AlbumResource.Uri;
+
+            CreateNowPlayingFile();
+
+            if (IsPlaying) {
+                StartTime = DateTime.Now;
+                timer1.Start();
+            }
+        }
+
+        private void AdUpdate() {
+            trackLink.Text = Spotify;
+            trackLink.Tag = "";
+            artistLink.Text = Spotify;
+            artistLink.Tag = "";
+            albumLink.Text = Spotify;
+            albumLink.Tag = "";
+            albumPicture.Image = null;
+            timer1.Stop();
+        }
+
+        #endregion
+
+        #region Log
+
+        #endregion
+    }
+}
