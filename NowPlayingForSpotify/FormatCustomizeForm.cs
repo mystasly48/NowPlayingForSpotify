def9dca1d39fa1d5a6417954c5935ea36d41391a
Maisu <mystasly48@gmail.com>
2016-09-30 22:46:04 +0900

    Updated: Support multiple language

--- NowPlayingForSpotify/FormatCustomizeForm.cs
+++ NowPlayingForSpotify/FormatCustomizeForm.cs
@@ -3,16 +3,15 @@ using System.Windows.Forms;
 using NowPlayingForSpotify.Properties;
 
 namespace NowPlayingForSpotify {
-    public partial class FormatCustomize : Form {
-        public FormatCustomize() {
+    public partial class FormatCustomizeForm : Form {
+        public FormatCustomizeForm() {
             InitializeComponent();
         }
 
         Settings settings = new Settings();
-        String defaultFormat = "%artist% - %track% #NowPlaying #Spotify %trackurl%";
-        String AppName = "NowPlaying for Spotify";
+        string defaultFormat = "%artist% - %track% #NowPlaying #Spotify %trackurl%";
 
-        private void FormatCustomize_Load(object sender, EventArgs e) {
+        private void FormatCustomizeForm_Load(object sender, EventArgs e) {
             textBox1.Text = settings.TweetFormat;
         }
 
@@ -26,18 +25,18 @@ namespace NowPlayingForSpotify {
 
         // Cancel
         private void button2_Click(object sender, EventArgs e) {
-            this.Close();
+            Close();
         }
 
         // Format
         private void button3_Click(object sender, EventArgs e) {
-            Format form = new Format();
+            var form = new Format();
             form.ShowDialog();
         }
 
         // Default
         private void button4_Click(object sender, EventArgs e) {
-            var result = MessageBox.Show("Are you sure you want to change back to the default format?", AppName, MessageBoxButtons.YesNo);
+            var result = MessageBox.Show(Resources.ChangeToDefaultFormat, Information.Name, MessageBoxButtons.YesNo);
             if (result == DialogResult.Yes) {
                 textBox1.Text = defaultFormat;
             }
@@ -47,9 +46,9 @@ namespace NowPlayingForSpotify {
             
         }
 
-        private void FormatCustomize_FormClosing(object sender, FormClosingEventArgs e) {
+        private void FormatCustomizeForm_FormClosing(object sender, FormClosingEventArgs e) {
             if (textBox1.Text != settings.TweetFormat) {
-                var result = MessageBox.Show("Are you sure you want to exit without applying your changes?", AppName, MessageBoxButtons.YesNo);
+                var result = MessageBox.Show(Resources.ExitWithoutApply, Information.Name, MessageBoxButtons.YesNo);
                 if (result == DialogResult.No) {
                     e.Cancel = true;
                 }
