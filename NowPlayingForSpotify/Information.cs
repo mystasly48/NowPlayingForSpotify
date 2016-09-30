da570ddc13b4af1a6b4f10b046d296f0b1ea2e3e
Maisu <mystasly48@gmail.com>
2016-09-30 21:23:10 +0900

    Added: SecretKeys, Information.

---
+++ NowPlayingForSpotify/Information.cs
@@ -0,0 +1,9 @@
+﻿namespace NowPlayingForSpotify {
+    public class Information {
+        public static readonly string Name = "NowPlaying for Spotify";
+        public static readonly string Developer = "Maisu";
+        public static readonly string Description = "";
+        public static readonly string Version = "";
+        public static readonly string RepositoryUrl = "http://github.com/mystasly48/NowPlayingForSpotify";
+    }
+}
